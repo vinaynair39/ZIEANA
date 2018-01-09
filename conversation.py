4134ab9f782fe6d8c05fce01621b2452297189a6
Vinay Nair <vnnair39@gmail.com>
2018-01-10 02:35:43 +0530

    conversation loop updated

--- conversation.py
+++ conversation.py
@@ -10,7 +10,7 @@ class Conversation:
     username = 'd6cafd45-923f-4f68-9bea-65a1167973ff'
     password = 'GBvL0jyr2BzP'
 
-    def convo(self, text):
+    def convo(self, text=None):
         conversation = ConversationV1(password=self.password, username=self.username, version='2017-05-26')
 
         response = conversation.message(
@@ -24,29 +24,37 @@ class Conversation:
         resp_text = response['output']['text'][0]
         return resp_text, response
 
-    def convoV2(self, text, response):
-        data = response['context']
-        data['user_name'] = 'vinay'
-        print(data)
+    def convoV2(self, user_name):
+
+        context = {}
+        context['user_name'] = user_name
         conversation = ConversationV1(password=self.password, username=self.username, version='2017-05-26')
+        while True:
+            user_input = input('>> ')
+            response = conversation.message(
+                workspace_id='e380de5b-7f5b-4287-beb4-3843f449de30',
+                input={
+                    'text': user_input
+                },
+                context=context,
 
-        response = conversation.message(
-            workspace_id='e380de5b-7f5b-4287-beb4-3843f449de30',
-            input={
-                'text': text
-            },
-            context=data,
+            )
 
-        )
-        # intent = response['intents'][0]['intent']
-        resp_text = response['output']['text'][0]
-        return resp_text
+            # If an intent was detected, print it to the console.
+            if response['intents']:
+                print('Detected intent: #' + response['intents'][0]['intent'])
+
+            if response['output']['text']:
+                print(response['output']['text'][0])
+
+            else:
+                return "damn! some error occurred!"
+
+            context = response['context']
+
+
 
 
 if __name__ == '__main__':
     test = Conversation()
-    data, resp = test.convo('book me a table')
-    print(data)
-    data = test.convoV2(input(), resp)
-    print(data)
-
+    test.convoV2('david')
